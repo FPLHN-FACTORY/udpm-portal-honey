52ad9674ae949dd7a7782500f30035c89d9af1f5
hungpvph25929 <hungpvph25929@fpt.edu.vn>
2023-03-09 01:45:30 +0700

    header + footer

--- front_end/src/route.js
+++ front_end/src/route.js
@@ -4,18 +4,6 @@ app.config(function ($routeProvider, $locationProvider) {
   $routeProvider
     .when("/home", {
       templateUrl: "pages/home.html",
-      controller: DongHoController,
-    })
-    .when("/home/detail/:id", {
-      templateUrl: "pages/home.html",
-      controller: DongHoController,
-    })
-    .when("/home/update/:id", {
-      templateUrl: "pages/home.html",
-      controller: DongHoController,
-    })
-    .when("/pages", {
-      templateUrl: "pages/pages.html",
     })
     .otherwise({
       redirectTo: "/home",
