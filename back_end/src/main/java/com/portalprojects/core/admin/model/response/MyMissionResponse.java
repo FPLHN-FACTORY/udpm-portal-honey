e9aafaade375cfbcffeec93b4d23349667148ad2
pohoang95@gmail.com <pohoang95@gmail.com>
2023-04-23 13:39:09 +0700

    commit_10_tamhvph_update_down_and_upload_file

---
+++ back_end/src/main/java/com/portalprojects/core/admin/model/response/MyMissionResponse.java
@@ -0,0 +1,6 @@
+package com.portalprojects.core.admin.model.response;
+
+
+@Projection(types = {SinhVien.class, ChuyenNganh.class, MonDatn.class, NhomDatn.class, GiangVienHuongDan.class, DotDangKy.class})
+public interface MyMission {
+}
