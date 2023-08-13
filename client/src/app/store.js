70b56b99049a75edaefae1e60629b91cb943a55a
xuanhai2907 <haipxph26772@fpt.edu.vn>
2023-08-13 20:46:00 +0700

    update table category

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -2,12 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import giftReducer from "./reducers/gift/gift.reducer";
 import conversionReducer from "./reducers/conversion/conversion.reducer";
 import honeyReducer from "./reducers/honey/honey.reducer";
-import semesterReducer from "./reducers/semester/semester.reducer"; 
+import semesterReducer from "./reducers/semester/semester.reducer";
 import historyReducer from "./reducers/history/history.reducer";
 import notificationReducer from "./reducers/notification/notification.reducer";
 import studentSemestertReducer from "./reducers/student-semester/student-semester.reducer";
 import usersReducer from "./reducers/users/users.reducer";
 import loaddingReducer from "./reducers/loading/loading.reducer";
+import categoryReducer from "./reducers/category/category.reducer";
 
 export const store = configureStore({
   reducer: {
@@ -20,6 +21,7 @@ export const store = configureStore({
     user: usersReducer,
     studentSemestert: studentSemestertReducer,
     loadding: loaddingReducer,
+    category: categoryReducer,
   },
 });
 
