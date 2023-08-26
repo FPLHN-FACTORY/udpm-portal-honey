4a3e61477e7cc98f775a4e3b0f59f0a6435b15da
Nguyễn Quốc Huy <huynq@blameo.com>
2023-08-27 03:07:46 +0700

    update project

--- honey-project/src/main/java/com/honeyprojects/core/common/MetaController.java
+++ honey-project/src/main/java/com/honeyprojects/core/common/MetaController.java
@@ -1,14 +1,13 @@
 package com.honeyprojects.core.common;
 
-import com.honeyprojects.infrastructure.contant.Constants;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class MetaController {
 
     @GetMapping("/version")
-    public String getVersion(Model model){
-        model.addAttribute("version", Constants.VERSION);
-        return "version";
+    public String getVersion() {
+        return "Không Bugs !!!";
     }
 }
