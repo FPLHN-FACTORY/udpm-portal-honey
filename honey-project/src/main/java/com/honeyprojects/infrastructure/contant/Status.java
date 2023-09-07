0f5576b4e894fda11359f217d869a48691374182
hungpvph25929 <hungpvph25929@fpt.edu.vn>
2023-09-07 22:01:32 +0700

    update project

--- honey-project/src/main/java/com/honeyprojects/infrastructure/contant/Status.java
+++ honey-project/src/main/java/com/honeyprojects/infrastructure/contant/Status.java
@@ -1,8 +1,6 @@
 package com.honeyprojects.infrastructure.contant;
 
 public enum Status {
-    CHO_PHE_DUYET,
-    DA_PHE_DUYET,
-    DA_HUY,
-    GUI_LAI_CHO_PHE_DUYET
+    HOAT_DONG,
+    KHONG_HOAT_DONG
 }
---
+++ honey-project/src/main/java/com/honeyprojects/infrastructure/exception/rest/ArticleProjectExceptionRestHandler.java
@@ -0,0 +1,16 @@
+package com.honeyprojects.infrastructure.exception.rest;
+
+import com.honeyprojects.infrastructure.exception.ArticleProjectExceptionHandler;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+public abstract class ArticleProjectExceptionRestHandler<Z extends Exception>
+        extends ArticleProjectExceptionHandler<ResponseEntity<?>,Z> {
+
+    @Override
+    protected ResponseEntity<?> wrap(Z ex) {
+        return new ResponseEntity<>(wrapApi(ex), HttpStatus.BAD_REQUEST);
+    }
+
+    protected abstract Object wrapApi(Z ex);
+}
