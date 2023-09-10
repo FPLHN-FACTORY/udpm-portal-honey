b6439c2f45155e6b3ddb7ca282ffb56880fcfd91
xuanhai2907 <haipxph26772@fpt.edu.vn>
2023-09-10 19:56:03 +0700

    demo ném lỗi logic ra ngoài fe thêm @Transactional vào các hàm thay đổi dữ liệu trong db sử dụng phương thức Patch cho các phương thức chỉ tác động đến 1 trường duy nhất trong đối tượng

--- honey-project/src/main/java/com/honeyprojects/infrastructure/contant/PropertyKeys.java
+++ honey-project/src/main/java/com/honeyprojects/infrastructure/contant/PropertyKeys.java
@@ -1,4 +1,9 @@
 package com.honeyprojects.infrastructure.contant;
 
 public class PropertyKeys {
+
+    private PropertyKeys() {
+    }
+    // viết các lỗi ở đây
+    public static final String CATEGORY_NOT_EXIST = "category.not.exist";
 }
