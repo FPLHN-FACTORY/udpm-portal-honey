543b4c47c858ba8c7ac4958cb3c01c2a5c0c120d
vinhnv01 <109213729+vinhnv01@users.noreply.github.com>
2023-10-04 11:07:50 +0700

    thiet ke giao dien phien dau gia

--- client/src/App.js
+++ client/src/App.js
@@ -34,6 +34,7 @@ import StArchive from "./pages/student/archive/StArchive";
 import RandomAddPoint from "./pages/censor/randomaddpoint/RandomAddPoint";
 import RequestConversionHistory from "./pages/censor/requestmanager/RequestConversionHistory";
 import ChestGift from "./pages/censor/chest-gift/ChestGift";
+import AuctionMangement from "./pages/censor/auction-management/AuctionManagement";
 
 function App() {
   return (
@@ -292,6 +293,16 @@ function App() {
                   <DashboardAuthUser>
                     <StArchive />
                   </DashboardAuthUser>
+                </AuthGuard>
+              }
+            />
+            <Route
+              path="/censor/auction-management"
+              element={
+                <AuthGuard>
+                  <DashboardCensor>
+                    <AuctionMangement />
+                  </DashboardCensor>
                 </AuthGuard>
               }
             />
