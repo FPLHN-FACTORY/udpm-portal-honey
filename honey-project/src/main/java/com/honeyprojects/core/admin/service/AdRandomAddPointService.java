1918e9c734bbf381cd25737e9382bb8b13de1a6b
xuanhai2907 <haipxph26772@fpt.edu.vn>
2023-10-07 15:54:12 +0700

    validate, import excel

--- honey-project/src/main/java/com/honeyprojects/core/admin/service/AdRandomAddPointService.java
+++ honey-project/src/main/java/com/honeyprojects/core/admin/service/AdRandomAddPointService.java
@@ -1,14 +1,15 @@
 package com.honeyprojects.core.admin.service;
 
-import com.honeyprojects.core.admin.model.request.AdminChestGiftRequest;
 import com.honeyprojects.core.admin.model.request.AdminRandomPointRequest;
+import com.honeyprojects.core.admin.model.response.AdminAddItemBO;
 import com.honeyprojects.core.admin.model.response.AdminCategoryResponse;
 import com.honeyprojects.core.admin.model.response.AdminChestGiftResponse;
 import com.honeyprojects.core.admin.model.response.AdminChestReponse;
-import com.honeyprojects.core.admin.model.response.AdminGiftResponse;
+import com.honeyprojects.core.admin.model.response.AdminAddItemDTO;
 import com.honeyprojects.core.common.response.SimpleResponse;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 public interface AdRandomAddPointService {
@@ -23,6 +24,10 @@ public interface AdRandomAddPointService {
     Boolean exportExcel();
 
     List<String> importExcel(MultipartFile file);
+
+    AdminAddItemBO previewDataImportExcel(MultipartFile file) throws IOException;
+
+    void importData(List<AdminAddItemDTO> lstAddItemDTO) throws IOException;
 
     List<AdminChestReponse> getAllChest();
 
