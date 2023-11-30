5f705ab65eea5c4833a6729791084b56869f43f8
Nguyễn Quốc Huy <huynq@blameo.com>
2023-11-30 12:41:18 +0700

    FIX: Call api event

--- honey-project/src/main/java/com/honeyprojects/core/admin/model/request/AdminAddPointStudentPortalEventsBOO.java
+++ honey-project/src/main/java/com/honeyprojects/core/admin/model/request/AdminAddPointStudentPortalEventsBOO.java
@@ -2,15 +2,32 @@ package com.honeyprojects.core.admin.model.request;
 
 import com.honeyprojects.core.common.base.PageableRequest;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.util.List;
 
 @Getter
 @Setter
-public class AdminAddPointStudentPortalEventsBOO extends PageableRequest {
-    private String code;
-    private List<String> lstStudentId;
-    private String categoryId;
+public class AdminAddPointStudentPortalEventsBOO {
+
     private Integer numberHoney;
+
+    private String categoryId;
+
+    private List<User> listUser;
+
+    private String moduleCode;
+
+    @Setter
+    @Getter
+    @NoArgsConstructor
+    public static class User {
+
+        private String id;
+
+        private String email;
+
+    }
+
 }
