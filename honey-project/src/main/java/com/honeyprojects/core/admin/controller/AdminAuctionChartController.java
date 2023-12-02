d3097b193ebefb9e9419043f6b1395df12c0877b
Nguyễn Quốc Huy <huynq@blameo.com>
2023-12-02 22:55:12 +0700

    FIX: Thống kê đấu giá

---
+++ honey-project/src/main/java/com/honeyprojects/core/admin/controller/AdminAuctionChartController.java
@@ -0,0 +1,28 @@
+package com.honeyprojects.core.admin.controller;
+
+import com.honeyprojects.core.admin.model.request.AdminAuctionChartLineRequest;
+import com.honeyprojects.core.admin.model.request.AdminAuctionChartTableRequest;
+import com.honeyprojects.core.admin.service.AdminAuctionChartLineService;
+import com.honeyprojects.core.common.base.ResponseObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/censor/auction/chart")
+public class AdminAuctionChartController {
+
+    @Autowired
+    private AdminAuctionChartLineService adminAuctionChartLineService;
+
+    @GetMapping("/line")
+    public ResponseObject chartLine(AdminAuctionChartLineRequest adminAuctionChartLineRequest) {
+        return new ResponseObject(adminAuctionChartLineService.getAuctionChartLine(adminAuctionChartLineRequest));
+    }
+
+    @GetMapping("/tables")
+    public ResponseObject chartTable(AdminAuctionChartTableRequest adminAuctionChartTableRequest) {
+        return new ResponseObject(adminAuctionChartLineService.getAuctionChartTable(adminAuctionChartTableRequest));
+    }
+}
