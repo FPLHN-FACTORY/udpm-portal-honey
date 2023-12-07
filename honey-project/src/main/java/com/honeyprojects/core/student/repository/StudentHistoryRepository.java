a5576dbefdf7c5cccd4ba8074383069f50d7a1ee
DESKTOP-NHQEQQF\LiamTrieu <gaupanda567@gmail.com>
2023-12-08 03:04:13 +0700

    fix bug giao dịch + làm lại history sinh viên

--- honey-project/src/main/java/com/honeyprojects/core/student/repository/StudentHistoryRepository.java
+++ honey-project/src/main/java/com/honeyprojects/core/student/repository/StudentHistoryRepository.java
@@ -4,6 +4,7 @@ import com.honeyprojects.core.student.model.param.StudentSumHistoryParam;
 import com.honeyprojects.core.student.model.request.StudentSearchHistoryRequest;
 import com.honeyprojects.core.student.model.response.StudentHistoryResponse;
 import com.honeyprojects.entity.History;
+import com.honeyprojects.entity.HistoryDetail;
 import com.honeyprojects.infrastructure.contant.TypeHistory;
 import com.honeyprojects.repository.HistoryRepository;
 import org.springframework.data.domain.Page;
@@ -36,15 +37,15 @@ public interface StudentHistoryRepository extends HistoryRepository {
 //           "and h.createdDate between :#{#param.formSemester} and :#{#param.toSemester}")
 //    Integer getTotalUseGift(StudentSumHistoryParam param);
 
-//    @Query("""
-//            select h from History h where h.studentId = :studentId
-//            and ((h.status = 1 and (:type is null or h.type = :type)) or (h.status = 2 and h.type = 3 and (:type is null or :type = 3) ))
-//            """)
-//    Page<History> getListHistory(String studentId, TypeHistory type, Pageable pageable);
+    @Query("""
+            select h from History h where h.studentId = :studentId and h.type <> 6
+            and ((h.status = 1 and (:type is null or h.type = :type)) or (h.status = 2 and h.type = 3 and (:type is null or :type = 3) ))
+            """)
+    Page<History> getListHistory(String studentId, TypeHistory type, Pageable pageable);
 
-//    @Query("""
-//            select h from History h where h.studentId = :studentId and
-//            h.status <> 1 and h.type <> 0 and (:type is null or h.type = :type)
-//            """)
-//    Page<History> getListRequest(String studentId, TypeHistory type, Pageable pageable);
+    @Query("""
+            select h from History h where h.studentId = :studentId and
+            h.status <> 1 and h.type <> 0 and (:type is null or h.type = :type)
+            """)
+    Page<History> getListRequest(String studentId, TypeHistory type, Pageable pageable);
 }
