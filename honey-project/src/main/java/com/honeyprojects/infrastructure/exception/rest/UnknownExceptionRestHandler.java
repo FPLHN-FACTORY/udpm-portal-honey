a3a1c93d00c83d33fa2d3546bfb0c0792dad9758
Nguyễn Quốc Huy <huynq@blameo.com>
2023-12-10 17:04:45 +0700

    FIX: Phê duyệt điểm

--- honey-project/src/main/java/com/honeyprojects/infrastructure/exception/rest/UnknownExceptionRestHandler.java
+++ honey-project/src/main/java/com/honeyprojects/infrastructure/exception/rest/UnknownExceptionRestHandler.java
@@ -1,13 +1,13 @@
-package com.honeyprojects.infrastructure.exception.rest;
-
-import org.springframework.web.bind.annotation.RestControllerAdvice;
-
-@RestControllerAdvice
-public final class UnknownExceptionRestHandler extends
-        ArticleProjectExceptionRestHandler<Exception> {
-
-    @Override
-    protected Object wrapApi(Exception ex) {
-        return ex.getMessage();
-    }
-}
+//package com.honeyprojects.infrastructure.exception.rest;
+//
+//import org.springframework.web.bind.annotation.RestControllerAdvice;
+//
+//@RestControllerAdvice
+//public final class UnknownExceptionRestHandler extends
+//        ArticleProjectExceptionRestHandler<Exception> {
+//
+//    @Override
+//    protected Object wrapApi(Exception ex) {
+//        return ex.getMessage();
+//    }
+//}
