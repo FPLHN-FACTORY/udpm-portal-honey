f9c33e32e82599a64e27beda24cf3a5943c28187
Nguyễn Quốc Huy <huynq@blameo.com>
2023-12-18 04:32:11 +0700

    FIX: đấu giá, mua vật phẩm

--- honey-project/src/main/java/com/honeyprojects/infrastructure/configution/CorsConfig.java
+++ honey-project/src/main/java/com/honeyprojects/infrastructure/configution/CorsConfig.java
@@ -1,17 +1,29 @@
 package com.honeyprojects.infrastructure.configution;
 
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.Arrays;
+
 @Configuration
-public class CorsConfig implements WebMvcConfigurer {
+public class CorsConfig implements CorsConfigurationSource {
+
+    @Value("${domain.socket}")
+    private String domainClient;
 
     @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
-                .allowedHeaders("Content-Type", "Authorization");
+    public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowedOrigins(Arrays.asList(domainClient));
+        corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "PATCH"));
+        corsConfiguration.setAllowedHeaders(Arrays.asList("Content-Type", "Authorization"));
+        corsConfiguration.setMaxAge(3600L);
+        corsConfiguration.setAllowCredentials(true);
+        return corsConfiguration;
     }
 }
